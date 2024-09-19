4209f5d0db969a33f557ef34136787c79e5fc53d
Gidrex-dev <gidrexm@gmail.com>
2024-09-19 11:45:19 +0300

    Обновление 1.1

--- Lab1.java
+++ Lab1.java
@@ -1,4 +1,5 @@
 class Lab1 { public static void main(String[] args) {
-    System.out.println("Нова пошта"); }
+    System.out.println("Нова пошта");
+    System.out.println("Нова строка");  }
     }
     
